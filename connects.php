1f4211d9bc7ef89a75f93cfb05bdbd971502eaeb
amirmahdiqa <66489247+amirmahdiqa@users.noreply.github.com>
2020-12-24 11:47:02 +0000

    Create connects.php

---
+++ connects.php
@@ -0,0 +1,14 @@
+<?php
+$SERVERNAME = "localhost";
+$username = "id15627086_amirmahdishahmiri";
+$password = "Pfc0V34ZX[LGT[XY";
+$dbname = "id15627086_amirmahdi";
+$connect = ("$SERVERNAME"."$username"."$password");
+if (!$connect){
+echo "خطا پیش آمده";	
+}
+else {
+echo "وصل شدید";	
+}
+
+?>
